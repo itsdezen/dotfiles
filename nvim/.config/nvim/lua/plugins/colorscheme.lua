4db026970c32d78fa7a1550a9a532d24e88e6285
Dezen <itsdezen@Dezens-Mac-mini.local>
2026-06-25 16:14:13 +0700

    🚀 make ghostty and nvim transparent

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,18 @@
 return {
-  {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    priority = 1000,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "github_dark",
-    },
-  },
+	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		priority = 1000,
+		opts = {
+			options = {
+				transparent = true,
+			},
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "github_dark",
+		},
+	},
 }
