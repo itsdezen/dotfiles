963e103790e4785507ffae5045567b1f6905158a
Dezen <hello@dezen.me>
2026-07-04 20:18:15 +0700

    🐞 use biome-check formatter to also apply lint fixes

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -4,12 +4,12 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { "biome", "prettier", stop_after_first = true },
-        javascriptreact = { "biome", "prettier", stop_after_first = true },
-        typescript = { "biome", "prettier", stop_after_first = true },
-        typescriptreact = { "biome", "prettier", stop_after_first = true },
-        json = { "biome", "prettier", stop_after_first = true },
-        css = { "biome", "prettier", stop_after_first = true },
+        javascript = { "biome-check", "prettier", stop_after_first = true },
+        javascriptreact = { "biome-check", "prettier", stop_after_first = true },
+        typescript = { "biome-check", "prettier", stop_after_first = true },
+        typescriptreact = { "biome-check", "prettier", stop_after_first = true },
+        json = { "biome-check", "prettier", stop_after_first = true },
+        css = { "biome-check", "prettier", stop_after_first = true },
         vue = { "prettier" },
         svelte = { "prettier" },
       },
