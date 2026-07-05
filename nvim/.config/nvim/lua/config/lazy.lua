ca2b18ba4937cba78e5fe5b07d6eb5ffd3b2a836
Dezen <hello@dezen.me>
2026-07-05 13:35:57 +0700

    🐞 fix nvim-dap setup crash by importing dap.core extra
    lang.python/go/typescript extras attach optional config to nvim-dap
    assuming dap.core is already imported; without it, lazy.nvim falls
    back to require("dap").setup(opts) which doesn't exist.

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.coding.blink" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.python" },
