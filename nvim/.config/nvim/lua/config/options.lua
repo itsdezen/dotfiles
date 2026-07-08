c9748c2fd68e4f33c809f7189b9372fcf88b8c08
Dezen <hello@dezen.me>
2026-07-08 18:07:22 +0700

    🎨 unify nvim diagnostic sign icons to same circle shape

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -16,10 +16,10 @@ vim.diagnostic.config({
   },
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = "✘",
-      [vim.diagnostic.severity.WARN]  = "▲",
-      [vim.diagnostic.severity.HINT]  = "●",
-      [vim.diagnostic.severity.INFO]  = "ℹ",
+      [vim.diagnostic.severity.ERROR] = "󰅙",
+      [vim.diagnostic.severity.WARN]  = "󰀨",
+      [vim.diagnostic.severity.HINT]  = "󰋗",
+      [vim.diagnostic.severity.INFO]  = "󰋼",
     },
   },
 })
